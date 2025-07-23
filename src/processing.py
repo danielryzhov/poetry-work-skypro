32224f8b0f192e1711afdbab2551387528d7911c
Даниэль Рыжов <karamba7771@gmail.com>
2025-07-23 15:55:30 +0300

    'end'

---
+++ src/processing.py
@@ -0,0 +1,21 @@
+from typing import Iterable
+from typing import Any
+
+
+def filter_by_state(filter_state: Iterable, state="EXECUTED") -> Any and Iterable:
+    """Функция принимает список словарей и возвращает новый список словарей содержащий только
+    те словари, у которых ключ state соответствует указанному значению"""
+
+    new_filter_state = []
+    for dictionary_state in filter_state:
+        if dictionary_state['state'] == state:
+            new_filter_state.append(dictionary_state)
+    return new_filter_state
+
+
+
+def sort_by_date(sort_date: Iterable, reverse=True) -> Any and Iterable:
+    sorted_date = sorted(sort_date, key=lambda dictionary_date: dictionary_date['date'], reverse=reverse)
+    return sorted_date
+
+
